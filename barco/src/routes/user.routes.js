097de84072fe063065f8e39195de6ebeeff1add9
Santiago <santti.72@hotmail.com.ar>
2024-08-01 16:56:41 -0300

    Cambios PRISMA

--- barco/src/routes/user.routes.js
+++ barco/src/routes/user.routes.js
@@ -1,16 +1,76 @@
-import {Router} from "express";
+import { Router } from "express";
 
 const router = Router();
 
+const users = [
+    { id: 1, nombre: "Cosme", apellido: "Fulanito" },
+    { id: 2, nombre: "Lalo", apellido: "Landa" },
+    { id: 3, nombre: "Mel", apellido: "Patiño" },
+    { id: 4, nombre: "Lisa", apellido: "Simpson" }
+]
 
-router.get("/", (request, response) =>{
+router.get("/", (request, response) => {
 
-    const user = {
-        nombre:"Cosme",
-        apellido:"Flanito"
-    }
-    response.json(user)
+    response.json(users)
+})
+
+
+router.get("/:id", (req,res)=>{
+    const { id } = req.params
+
+    const user = users.find(user => user.id === Number(id))
+
+    if(!user) return res.status(404).json({ error: "User NOT found"})
+
+    res.json(user)
+})
+
+
+router.post("/", (req, res) => {
+
+    const { id, nombre, apellido } = req.body;
+
+    if ( !id || !nombre || !apellido) return res.status(404).json( {error: "FALTAN DATOS"}); 
+
+    users.push({
+        id,
+        nombre,
+        apellido
+    })
+    res.json({ message: "User created", user: {id, nombre, apellido}})
 
 })
+
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, apellido } = req.body
+
+    if (!id || !nombre || !apellido) return res.status(400).json({ error: "Faltan datos" })
+
+    const userExists = users.find(user => user.id === Number(id))
+
+    if(!userExists) return res.status(404).json({ error: "User not found" })
+
+    const index = users.findIndex(user => user.id === Number(id))
+    users[index] = { id: Number(id), nombre, apellido }
+
+    res.json({ message: "User updated", user: { id: Number(id), nombre, apellido }})
+})
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    const userExists = users.find(user => user.id === Number(id))
+
+    if(!userExists) return res.status(404).json({ error: "User not found" })
+
+    const index = users.findIndex(user => user.id === Number(id))
+    
+    users.splice(index, 1)
+
+    res.json({ message: "User deleted", user: userExists })
+})
+
 
 export default router;
--- barco/src/server.js
+++ barco/src/server.js
@@ -5,10 +5,12 @@ const app = express()
 
 const PORT = 5000;
 
+app.use(express.json());
+app.use(express.urlencoded( { extended: true} ));
+
 app.use("/api/users", userRouter)
+
 
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`);
 })
-
-
