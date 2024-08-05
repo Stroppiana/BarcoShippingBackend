f0129bb68c44d1d97a75d26bf03eceaea06a527b
Santiago <santti.72@hotmail.com.ar>
2024-08-05 16:30:34 -0300

    conexion con fetch

--- barco/src/routes/auth.routes.js
+++ barco/src/routes/auth.routes.js
@@ -1,9 +1,40 @@
 import { Router } from "express";
-import { getUserByEmail } from "../services/user.service.js";
+import { getUserByEmail, createUser } from "../services/user.service.js";
 import { generateAccesToken } from "../utils/helpers.js";
 import { Rol } from "@prisma/client";
 
 const router = Router();
+
+router.post("/register", async (req, res) =>{
+
+    const { email, name, password} = req.body;
+
+    if ( !email || !name || !password) return res.status(400).json( {message: "Todos los datos son obligatorios"})
+
+    try{
+
+        const user = await createUser({ email, name, password, rol: Rol.USER });
+
+        const token = generateAccesToken(email, user.rol);
+
+        res.json({
+            user: {
+             email: user.email,
+             name: user.name,
+             password: user.password,
+             rol: user.rol
+            },
+            token
+         }) 
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message: "ERROR CRITICO"})
+    }
+
+})
+
+
 
 router.post("/login", async (req, res) =>{
     const { email, password } = req.body;
--- barco/src/server.js
+++ barco/src/server.js
@@ -1,19 +1,25 @@
 import express from "express";
-import userRouter from "./routes/user.routes.js"
-import authRouter from "./routes/auth.routes.js"
+import cors from "cors";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 import validateToken from "./utils/helpers.js"
-
-const app = express()
+import dotenv from 'dotenv';
+dotenv.config(); 
+const app = express();
 
 const PORT = 5000;
 
 app.use(express.json());
-app.use(express.urlencoded( { extended: true} ));
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/users", validateToken ,userRouter)
-app.use("/api/auth", authRouter)
+app.use(cors({
+    origin: 'http://localhost:5173' 
+}));
 
+app.use('/api/auth', authRouter);
+
+app.use("/api/users", /*validateToken,*/ userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`);
-})
+});
