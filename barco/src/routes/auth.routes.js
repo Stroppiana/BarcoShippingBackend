0816816c409fee6fc17fb92014a46f11d107b7b0
Santiago <santti.72@hotmail.com.ar>
2024-08-08 17:31:31 -0300

    Detalles finales

--- barco/src/routes/auth.routes.js
+++ barco/src/routes/auth.routes.js
@@ -2,8 +2,42 @@ import { Router } from "express";
 import { getUserByEmail, createUser } from "../services/user.service.js";
 import { generateAccesToken } from "../utils/helpers.js";
 import { Rol } from "@prisma/client";
+// import bcrypt from 'bcrypt';
+
+// const saltRounds = 10;
 
 const router = Router();
+
+// router.post("/register", async (req, res) =>{
+
+//     const { email, name, password} = req.body;
+
+//     if ( !email || !name || !password) return res.status(400).json( {message: "Todos los datos son obligatorios"})
+
+//     try{
+
+//         const passwordHash = await bycript.hash(password, saltRounds);
+
+//         const user = await createUser({ email, name, password: passwordHash, rol: Rol.USER });
+
+//         const token = generateAccesToken(email, user.rol);
+
+//         res.json({
+//             user: {
+//              email: user.email,
+//              name: user.name,
+//              password: user.password,
+//              rol: user.rol
+//             },
+//             token
+//          }) 
+
+//     }catch(error){
+//         console.log(error)
+//         res.status(500).json({message: "ERROR CRITICO"})
+//     }
+
+// })
 
 router.post("/register", async (req, res) =>{
 
@@ -12,6 +46,7 @@ router.post("/register", async (req, res) =>{
     if ( !email || !name || !password) return res.status(400).json( {message: "Todos los datos son obligatorios"})
 
     try{
+
 
         const user = await createUser({ email, name, password, rol: Rol.USER });
 
@@ -46,7 +81,11 @@ router.post("/login", async (req, res) =>{
 
         if(!user) return res.status(404).json( { message: "Usuario NO existe" })
 
-        if(user.password != password) return res.status(404).json( { message: "Clave incorrecta" })
+       // const match = await bcrypt.compare(password, user.password);
+
+       // if(!match) return res.status(401).json( { message: "Clave incorrecta" })
+
+        if(user.password != password) return res.status(401).json( { message: "Clave incorrecta" })
 
         const token = generateAccesToken(email, user.rol);
 
